d7e88b1ae50e4a7319fb11d7c813a2fcb14122c4
AutumNPROGRAM <125322182+AutumNPROGRAM@users.noreply.github.com>
2023-11-07 19:07:01 +0700

    Add files via upload

---
+++ www/js/script.js
@@ -0,0 +1,13 @@
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
